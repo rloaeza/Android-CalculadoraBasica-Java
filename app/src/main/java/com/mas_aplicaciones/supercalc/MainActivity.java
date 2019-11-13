8a165f7f8886f8ca9e014f14e8d8089e88bc5560
Roberto Loaeza Valerio <roberto.loaeza@gmail.com>
2019-11-12 19:18:57 -0600

    Version 1

---
+++ app/src/main/java/com/mas_aplicaciones/supercalc/MainActivity.java
@@ -0,0 +1,80 @@
+package com.mas_aplicaciones.supercalc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.mariuszgromada.math.mxparser.Expression;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText Display;
+    String memoria = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Display = findViewById(R.id.Display);
+
+        final Button botonMemoria = findViewById(R.id.botonMem);
+        botonMemoria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if ( memoria.isEmpty() ) {
+                    memoria = Display.getText().toString();
+                    Display.setText("");
+                    botonMemoria.setText(memoria);
+                }
+                else {
+                    Display.append(memoria);
+                    memoria = "";
+                    botonMemoria.setText("MEM");
+                }
+            }
+        });
+
+    }
+
+    public void agregarValor(View vista) {
+        Button boton = (Button) vista;
+        Display.append(boton.getText());
+    }
+
+    public void agregarFuncion(View vista) {
+        Button boton = (Button) vista;
+        Display.setText(boton.getText() + "(" + Display.getText() + ")");
+    }
+
+    public void limpiarEntrada(View vista) {
+        Display.setText("");
+    }
+
+    public void funcionIgual(View vista) {
+        Double resultado = new Expression(Display.getText().toString()).calculate();
+        Display.setText(String.valueOf(resultado));
+    }
+    public void agregarPunto(View vista) {
+        String texto = Display.getText().toString();
+        if( texto.isEmpty()  ) {
+            return;
+        }
+        if( texto.endsWith(".") ) {
+            return;
+        }
+
+        Display.append(".");
+    }
+    public void limpiarTodo(View vista) {
+        if( !memoria.isEmpty() ) {
+            memoria = "";
+            Button botonMemoria = findViewById(R.id.botonMem);
+            botonMemoria.setText("MEM");
+        }
+        Display.setText("");
+    }
+
+}
